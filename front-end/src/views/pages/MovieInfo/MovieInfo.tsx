2234067ecb5c8126ee22092d405258e654ae4463
Roudi22 <roudi.alzkemi@gmail.com>
2024-10-21 10:55:21 +0200

    start the component

--- front-end/src/views/pages/MovieInfo/MovieInfo.tsx
+++ front-end/src/views/pages/MovieInfo/MovieInfo.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 const MovieInfo: React.FC = () => {
+  const [movieInfo, setMovieInfo] = React.useState<object>({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/movie/${id}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [ id ]);
   return (
     <div>
       <h1>Movie Info Page</h1>
       <p>This is the movie information page</p>
+      <p>Movie ID: {movieInfo._id}</p>
     </div>
   );
 };
